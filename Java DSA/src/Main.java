a779ed76807d4bd6995a32fb09e91c7f6f002dcf
AgcFJCTY <ashishrawat82874@gmail.com>
2024-11-04 19:01:58 +0530

    Learning git

---
+++ Java DSA/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] ros;
+        ros = new int[5];
+        System.out.println(ros[0]);
+        String [] arr = new String[5];
+        System.out.println(arr[0]);
+    }
+}
